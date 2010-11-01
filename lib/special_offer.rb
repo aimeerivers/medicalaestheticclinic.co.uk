f34ee842262ab337e38fee654878538edbb8bbb0
aimee daniells <aimeedaniells@googlemail.com>
2010-11-01 23:26:38 +0000

    November special offer - Fire & Ice / Jan Marini.
    Enable easier maintenance of special offers.

---
+++ lib/special_offer.rb
@@ -0,0 +1,19 @@
+class SpecialOffer
+
+  def self.title
+    "November Offer"
+  end
+
+  def self.short_description
+    "FREE Fire &amp; Ice Facial when you purchase ANY three of our Jan Marini products"
+  end
+
+  def self.link
+    '/products/is-clinical#fire-and-ice'
+  end
+
+  def self.small_image
+    '/images/products/is-clinical/fire-and-ice-88.jpg'
+  end
+
+end
