3c2bf1ad34e3d697073a1f3afdff7b8ac3ec59ca
aimee daniells <aimeedaniells@googlemail.com>
2011-11-02 17:57:45 +0000

    Full stops are nice.

--- lib/special_offer.rb
+++ lib/special_offer.rb
@@ -5,7 +5,7 @@ class SpecialOffer
   end
 
   def self.short_description
-    "20% off on any ONE Jan Marini skin care product"
+    "20% off on any ONE Jan Marini skin care product."
   end
 
   def self.link
